8b827525fc8b20283fd6fb59d25e83d827051d2c
hf-chow <honfungchow@gmail.com>
2026-06-08 08:45:20 +0800

    add graphql

--- src/provider/runpod.rs
+++ src/provider/runpod.rs
@@ -1,4 +1,5 @@
 use super::*;
+use anyhow::{Ok, anyhow};
 use async_trait::async_trait;
 use serde::Deserialize;
 
@@ -14,5 +15,24 @@ impl RunPodClient {
             api_key: api_key,
             client: client,
         }
+    }
+
+    async fn graphql(
+        &self,
+        query: &str,
+        variables: serde_json::Value,
+    ) -> Result<serde_json::Value> {
+        let resp = self
+            .client
+            .post("https://api.runpod.io/graphql")
+            .bearer_auth(&self.api_key)
+            .json(&serde_json::json!({"query": query, "variables": variables}))
+            .send()
+            .await?;
+        let mut body: serde_json::Value = resp.json().await?;
+        if !body["errors"].is_null() {
+            anyhow::bail!("error found from response: {}", body["errors"])
+        }
+        Ok(body["data"].take())
     }
 }
