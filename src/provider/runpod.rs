b8697d80730128605303c2284ccda276e802f649
hf-chow <honfungchow@gmail.com>
2026-06-11 08:47:00 +0800

    impl list offers

--- src/provider/runpod.rs
+++ src/provider/runpod.rs
@@ -2,6 +2,7 @@ use super::*;
 use anyhow::{Ok, anyhow};
 use async_trait::async_trait;
 use serde::Deserialize;
+use serde_json::json;
 
 struct RunPodClient {
     api_key: String,
@@ -52,5 +53,51 @@ impl RunPodClient {
             anyhow::bail!("error found from response: {}", body["errors"])
         }
         Ok(body["data"].take())
+    }
+
+    async fn list_offers(&self, criteria: SearchCriteria) -> Result<Vec<NormalizedOffer>> {
+        let query = r#"
+        query{
+            gpuTypes {
+                id
+                displayName
+                memoryInGb
+                lowestPrice(input: { gpuCount: 1}) { minimumBidPrice }
+            }
+        }"#;
+        let data = self.graphql(query, json!({})).await?;
+        let parsed: GpuTypesData = serde_json::from_value(data)?;
+
+        let mut offers = parsed
+            .gpu_types
+            .iter()
+            .filter_map(|g| {
+                if !g.lowest_price.is_none() {
+                    Some(NormalizedOffer {
+                        provider: "runpod".to_string(),
+                        gpu_name: g.display_name,
+                        num_gpus: 1,
+                        vram_gb: g.memory_in_gb,
+                        price_per_hour: g.lowest_price.as_ref()?.minimum_bid_price?,
+                        location: None,
+                        provider_ref: g.id.clone(),
+                        reliability: None,
+                    })
+                } else {
+                    None
+                }
+            })
+            .filter(|o| {
+                if o.vram_gb >= criteria.min_vram_gb
+                    && o.price_per_hour <= criteria.max_price_per_hour
+                {
+                    Some(o)
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+        offers.sort_by(|a, b| a.price_per_hour.cmp(&b.price_per_hour));
+        Ok(offers)
     }
 }
